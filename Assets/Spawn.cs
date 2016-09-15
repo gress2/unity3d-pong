c96e44ba6c5adaf3469951eda4c89350d580a3a8
Collin Gress <gress126@gmail.com>
2016-09-14 23:06:54 -0500

    initial commit

---
+++ Assets/Spawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    private bool ballInPlay;
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+        ballInPlay = false;
+        reset();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void reset()
+    {
+        Instantiate(ball, transform.position, transform.rotation);
+    }
+}
